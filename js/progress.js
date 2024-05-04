56fb58e91d9704b3b9b9adefc7040eaf57d627ae
IrinaPeshko <privet-kira1999@mail.ru>
2024-05-04 17:55:23 +0300

    feat: create welcome page

--- js/progress.js
+++ js/progress.js
@@ -13,7 +13,7 @@ export const progress = (() => {
         loaded += 1;
 
         bar.style.width = Math.min((loaded / total) * 100, 100).toString() + "%";
-        info.innerText = `Loading assets (${loaded}/${total}) [${parseInt((loaded / total) * 100).toFixed(0)}%]`;
+        info.innerText = `Loading ${parseInt((loaded / total) * 100).toFixed(0)}%`;
 
         if (loaded == total) {
             util.show();
